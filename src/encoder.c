c628d8d08132b261632c59a9668300e5f359d22c
Kevin Langer <klanger@ccis.neu.edu>
2013-04-02 04:11:41 -0300

    Create encoder.c
    Create the empty encoder API

---
+++ src/encoder.c
@@ -0,0 +1,9 @@
+/*The encoder accepts the location of a chunk, 
+  encodes it, and returns the location of the 
+  encoded chunk 
+*/
+
+chunk_t encoder(chunk_t* pThis) {
+    //Encode data at a chunk granularity         
+    return chunk_t* pThis;  
+}
