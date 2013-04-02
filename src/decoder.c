157fb50f12050087ac0c2116ccbf53528f38b59b
Kevin Langer <klanger@ccis.neu.edu>
2013-04-02 04:45:16 -0300

    Chunk specificity about directorion of decoding

--- src/decoder.c
+++ src/decoder.c
@@ -3,7 +3,7 @@ decodes it, and returns the location of the
 decoded chunk
 */
 
-chunk_t decoder(chunk_t* pThis) {
+chunk_t decoder(encodedChunk_t* pThis) {
     //Decode data at a chunk granularity
     return chunk_t* pThis;
 }
