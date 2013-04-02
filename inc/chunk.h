97a70376a92e3aa0075536a1f4190814d4f1e4e2
Kevin Langer <klanger@ccis.neu.edu>
2013-04-02 04:20:15 -0300

    Create chunk.h

---
+++ inc/chunk.h
@@ -0,0 +1,15 @@
+#ifndef _CHUNK_H_
+#define _CHUNK_H_
+
+#define SAMPLE_SIZE                 (1024*5)
+
+typedef struct {
+  /* define a union to have different acess to same data in chunk */
+  union {
+    unsigned char       u08_buff[SAMPLE_SIZE];  /** Unsigned Data Chunk */
+    unsigned short      u16_buff[SAMPLE_SIZE/2];
+    unsigned int        u32_buff[SAMPLE_SIZE/4];
+    signed char         s08_buff[SAMPLE_SIZE];  /** Signed Data Chunk */
+    signed short        s16_buff[SAMPLE_SIZE/2];
+    signed int          s32_buff[SAMPLE_SIZE/4];
+  };
