608973a85a161b2e44be8cad1948160ba8b252a1
Kevin Langer <klanger@ccis.neu.edu>
2013-04-02 04:22:16 -0300

    Create decoder.c

---
+++ src/decoder.c
@@ -0,0 +1,9 @@
+/*The decoder accepts the location of a chunk,
+decodes it, and returns the location of the
+decoded chunk
+*/
+
+chunk_t decoder(chunk_t* pThis) {
+    //Decode data at a chunk granularity
+    return chunk_t* pThis;
+}
