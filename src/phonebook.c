bcb6add531ed4ec5284d04c8a20ff2bc3b9ef47a
PAL <pal@tllHost.localdomain>
2013-04-02 13:21:00 -0400

    changing import to include, adding return value to phonebook

--- src/phonebook.c
+++ src/phonebook.c
@@ -1,6 +1,7 @@
-#import "phonebook.h"
+#include "phonebook.h"
 
 return_value_t phonebook_init(phonebook_t* pThis, zhone_address_t address1, zhone_address_t address2) {
     pThis->address1 = address1;
     pThis->address2 = address2;
+    return PASS;
 }
