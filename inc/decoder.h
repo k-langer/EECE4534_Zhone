9b1675143279b65ae2633d2caaadc32a631d5898
Kevin Langer <klanger@ccis.neu.edu>
2013-04-09 21:55:37 -0300

    API update to reflect implementation and uniformity

--- inc/decoder.h
+++ inc/decoder.h
@@ -3,6 +3,17 @@
 
 #include "commonTypes.h"
 
-audioChunk_t* decoder( dataChunk_t* pThis );
+typedef struct decoder {
+    int nbBytes; 
+    SpeexBits bits;     
+    void* status; 
+    //char * input;   
+    float * output;
+    int outputSize;
+} decoder_t; 
+
+int decoder_init(decoder_t* pThis, int nbBytes);
+int decoder_decode(decoder_t* pThis, chunk_t* pDataChunk, chunk_t* pAudioChunk);
+
 
 #endif
