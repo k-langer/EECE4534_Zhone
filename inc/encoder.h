8f24191bf2de1a39b591cf7257b3e3fd7e2dc91e
Kevin Langer <klanger@ccis.neu.edu>
2013-04-09 21:53:51 -0300

    Update encoder.h
    Updated API to more accurately interface with implementation and follow form of all other classes.

--- inc/encoder.h
+++ inc/encoder.h
@@ -3,6 +3,17 @@
 
 #include "commonTypes.h"
 
-dataChunk_t* encoder( audioChunk_t* pThis );
+#define QUALITY 8
+
+typedef struct encoder {
+    int nbBytes; 
+    SpeexBits bits;     
+    void* status; 
+    float * input;   
+    int outputSize;
+} encoder_t; 
+
+int encoder_init(encoder_t* pThis);
+int encoder_encode(encoder_t* pThis, chunk_t* pAudioChunk, chunk_t* pDataChunk );
 
 #endif
