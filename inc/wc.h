bc485151cdefa4d388fa08c52c1cb022b4252c33
Todd Lunter <tlunter@gmail.com>
2013-04-15 13:03:02 -0400

    Wrote most of the Wireless Communicator, just need to finish parsing the input in our message system and define the bytes

--- inc/wc.h
+++ inc/wc.h
@@ -32,6 +32,7 @@ typedef struct {
     bufferPool_t *pBufPool;
     wire_t wire;
     xbee_t xbee;
+    unsigned short to;
 } wc_t;
 
 /** Initializes the wireless communicator
--- src/wc.c
+++ src/wc.c
@@ -30,22 +30,29 @@ return_value_t Wc_Init( wc_t *pThis, bufferPool_t *pBufPool, isrDisp_t *pIsrDisp
 
     Wire_Init(&pThis->wire, &pThis->rx, &pThis->tx, &pThis->pBufPool);
     Xbee_Init(&pThis->xbee, &pThis->rx, &pThis->tx, &pThis->pBufPool);
+
+    return PASS;
 }
 
 /** Starts the wireless communicator
  *
  * @return PASS on success, FAIL otherwise
  */
-return_value_t Wc_Start( void )
+return_value_t Wc_Start( wc_t *pThis, void )
 {
     uartRx_start(&pThis->rx);
+
+    return PASS;
 }
 
 /** Stops the wireless communicator
  *
  * @return PASS on success, FAIL otherwise
  */
-return_value_t Wc_Stop( void );
+return_value_t Wc_Stop( wc_t *pThis, void )
+{
+    return PASS;
+}
 
 /** Sets who to target with the messages
  *
@@ -53,19 +60,46 @@ return_value_t Wc_Stop( void );
  *
  * @return PASS on sucess, FAIL otherwise
  */
-return_value_t Wc_SetDestination( unsigned short to );
+return_value_t Wc_SetDestination( wc_t *pThis, unsigned short to )
+{
+    pThis->to = to;
+    
+    return PASS;
+}
 
 /** Sends an initialize call message
  *
  * @return PASS on success, FAIL otherwise
  */
-return_value_t Wc_InitiateCall( void );
+return_value_t Wc_InitiateCall( wc_t *pThis, void )
+{
+    chunk_t *pChunk = NULL;
+    if (PASS == bufferPool_acquire(pThis->pBufPool, &pChunk))
+    {
+        pChunk->u08_buff[0] = INITIATE_CALL_BYTE;
+        pChunk->bytesUsed = 1;
+
+        return Wire_Send(&pThis->wire, pChunk);
+    }
+    return FAIL;
+}
 
 /** Sends an accept call message
  *
  * @return PASS on success, FAIL otherwise
  */
-return_value_t Wc_AcceptCall( void );
+return_value_t Wc_AcceptCall( wc_t *pThis, void )
+{
+    chunk_t *pChunk = NULL;
+    if (PASS == bufferPool_acquire(pThis->pBufPool, &pChunk))
+    {
+        pChunk->u08_buff[0] = ACCEPT_CALL_BYTE;
+        pChunk->bytesUsed = 1;
+
+        return Wire_Send(&pThis->wire, pChunk);
+    }
+    return FAIL;
+}
 
 /** Sends a data message with the given data
  *
@@ -73,19 +107,53 @@ return_value_t Wc_AcceptCall( void );
  *
  * @return PASS on success, FAIL otherwise
  */
-return_value_t Wc_Send( chunk_t *pChunk );
+return_value_t Wc_Send( wc_t *pThis, chunk_t *pChunk )
+{
+    int i;
+    int len = pChunk->bytesUsed;
+    for (i = len - 1; i >= 0; i--)
+    {
+        pChunk->u08_buff[i+1] = pChunk->u08_buff[i];
+    }
+    pChunk->u08_buff[0] = DATA_BYTE;
+    pChunk->bytesUsed = 1 + len;
+
+    return Wire_Send(&pThis->wire, pChunk);
+}
 
 /** Sends an end call message
  *
  * @return PASS on success, FAIL otherwise
  */
-return_value_t Wc_EndCall( void );
+return_value_t Wc_EndCall( wc_t *pThis, void )
+{
+    chunk_t *pChunk = NULL;
+    if (PASS == bufferPool_acquire(pThis->pBufPool, &pChunk))
+    {
+        pChunk->u08_buff[0] = END_CALL_BYTE;
+        pChunk->bytesUsed = 1;
+
+        return Wire_Send(&pThis->wire, pChunk);
+    }
+    return FAIL;
+}
 
 /** Sends a reject call message
  *
  * @return PASS on sucess, FAIL otherwise
  */
-return_value_t Wc_RejectCall( void );
+return_value_t Wc_RejectCall( wc_t *pThis, void )
+{
+    chunk_t *pChunk = NULL;
+    if (PASS == bufferPool_acquire(pThis->pBufPool, &pChunk))
+    {
+        pChunk->u08_buff[0] = REJECT_CALL_BYTE;
+        pChunk->bytesUsed = 1;
+
+        return Wire_Send(&pThis->wire, pChunk);
+    }
+    return FAIL;
+}
 
 /** Gets a message from the communicator
  *
@@ -93,4 +161,8 @@ return_value_t Wc_RejectCall( void );
  *
  * @return PASS on success, FAIL otherwise
  */
-return_value_t Wc_Receive( chunk_t *pChunk );
+return_value_t Wc_Receive(a wc_t *pThis, chunk_t *pChunk )
+{
+    /* Check for status update or data */
+    return PASS;
+}
