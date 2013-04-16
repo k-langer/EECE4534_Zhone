4ec5582b81d6a9056db84802e7ec9471cbe1f442
JuanAnelo <antelo.juan@gmail.com>
2013-04-15 22:23:09 -0400

    Fixed interrupts
    Issue: display_init() is preventing interrupts

--- inc/ui.h
+++ inc/ui.h
@@ -10,13 +10,13 @@
         -allow getting and setting of status
         -update status and LED output through ISRs
 
-	To Do: 
-	- After a call ends, status needs to be set back to idle, this is not detected by an ISR 
-	- After a user hangs up, the status needs to be set back to idle (it is currently set to END_CALL) 
+	To Do:
+	- After a call ends, status needs to be set back to idle, this is not detected by an ISR
+	- After a user hangs up, the status needs to be set back to idle (it is currently set to END_CALL)
 ***/
 
 typedef struct {
-    
+
 	phone_status_t status;
 } ui_t;
 
@@ -24,38 +24,39 @@ typedef struct {
     init
         -initialize status to IDLE
         -register ISRs
-        
+
         *pThis - pointer to ui object to initialize
-        
+
         return PASS for success or FAIL for failure
 ***/
-//static isrDisp_t isrDisp; //this will be created in Zhone.c 
+//static isrDisp_t isrDisp; //this will be created in Zhone.c
 
-return_value_t ui_init( ui_t* pThis, isrDisp_t* isrDisp );  
+void ui_init( ui_t* pThis, isrDisp_t* isrDisp );
+//void ui_init(void);
 
 /***
     get_status
-    
+
         pThis - pointer to ui object to grab status from
-    
+
         returns status of *pThis
 ***/
 phone_status_t ui_get_status(ui_t* pThis);
 
-/*** 
+/***
     set_status
-    
-        pThis - pointer to ui object whose status will be set 
+
+        pThis - pointer to ui object whose status will be set
         newStatus - status that will be set on *pThis
 
         returns PASS for success or FAIL for failure
 ***/
-return_value_t ui_set_status(ui_t* pThis, phone_status_t newStatus);
+void ui_set_status(ui_t* pThis, phone_status_t newStatus);
 
 /***
     button1_ISR & button2_ISR
         -check status to determine state of system
-        -update status        
+        -update status
         -update LEDs for user
 
         pThis - pointer to ui object to be updated
