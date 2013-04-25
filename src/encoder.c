e793b690b94044c2c81ac2c63b61b437f143bf1e
Kevin Langer <klanger@ccis.neu.edu>
2013-04-25 17:43:41 -0300

    Comments

--- src/encoder.c
+++ src/encoder.c
@@ -28,16 +28,19 @@ int encoder_encode( encoder_t* pThis, chunk_t* pAudioChunk, chunk_t* pDataChunk)
     unsigned char excCh = 1<<(8*sizeof(char)-1); 
     for (i=0; i < CHUNK_SIZE; i+=SAMPLE_DIV) {
       smallSample = (char) (pAudioChunk->s16_buff[i] >> SHIFT);   
+      /*If the contigious silence ends or is larger than the sample 
+        storage for samples, then encode the silence as a MSB of 1 and a length of silence
+      */
       if ( (quietSamples!=0 && smallSample != QUIET_FLOOR) || quietSamples == excCh-1 ) {
-         pDataChunk->s08_buff[count] = quietSamples | excCh; 
+         pDataChunk->s08_buff[count++] = quietSamples | excCh; 
          quietSamples = 0;  
-         count++; 
       } 
       if ( smallSample == QUIET_FLOOR ) {
          quietSamples++; 
       } else {
-         pDataChunk->s08_buff[count] = smallSample>>1; 
-         count++;
+         /*If it is not silence, encode it into the data chunk and shift over 1 more, as 
+           the MSB is only used to indiciate a special sample and not a data sample */
+         pDataChunk->s08_buff[count++] = smallSample>>1;
       } 
     }
     pDataChunk->bytesUsed = count;  
